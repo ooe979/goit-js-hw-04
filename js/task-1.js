5e916836b3646e7362c3b756010c351f3e3fe4dc
Oleksandr <165651013+ooe979@users.noreply.github.com>
2025-05-29 19:01:03 +0300

    Update task-1.js
    Add task

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,27 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-// makeTransaction();
+// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться 
+// всі товари в контейнер при пакуванні.
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// Функція оголошує два параметри:
+
+// products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів.
+// Наприклад, { apples: 2, grapes: 4 }.
+// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну 
+  // кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
